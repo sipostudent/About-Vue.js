ac37a1dce2216838fdc9ea082fb220168dbec6b3
Sipo Charles <sipocharles@yahoo.co.uk>
2020-10-16 15:08:56 +0100

    Exploring Event Modifiers

--- content/section-02-basics/basics-03-events-starting-code/app.js
+++ content/section-02-basics/basics-03-events-starting-code/app.js
@@ -2,10 +2,17 @@ const app = Vue.createApp({
   data() {
     return {
       counter: 0,
-      name: ''
+      name: '',
+      confirmedName: ''
     };
   },
   methods: {
+    confirmInput() {
+      this.confirmedName = this.name;
+    },
+    submitForm() {
+      alert('Submitted!');
+    },
     setName(event, lastName) {
       this.name = event.target.value + ' ' + lastName;
     },
