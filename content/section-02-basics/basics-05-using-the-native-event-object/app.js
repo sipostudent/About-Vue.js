c4404a6ffd008bdb4360fb1f0d15e4c1038fbf53
Sipo Charles <sipocharles@yahoo.co.uk>
2020-10-17 11:07:03 +0100

    Introducing Computed Properties

--- content/section-02-basics/basics-05-using-the-native-event-object/app.js
+++ content/section-02-basics/basics-05-using-the-native-event-object/app.js
@@ -5,6 +5,15 @@ const app = Vue.createApp({
       name: ''
     };
   },
+  computed: {
+    fullname() {
+      console.log('Running again...');
+      if (this.name === '') {
+        return '';
+      }
+      return this.name + ' ' + 'Charles';
+    }
+  },
   methods: {
     outputFullname() {
       console.log('Running again...');
