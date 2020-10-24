e3c0d1997aaeb0b404a9841978d8818e3db578a7
Sipo Charles <sipocharles@yahoo.co.uk>
2020-10-24 23:50:55 +0100

    README.md

---
+++ content/section-04-course-project-the-monster-slayer/prj-monster-01-starting-setup/app.js
@@ -0,0 +1,15 @@
+const app = Vue.createApp({
+  data() {
+    return {
+      playerHealth: 100,
+      monsterHealth: 100
+    };
+  },
+  methods: {
+    attackMonster() {
+      Math.floor(Math.random() * (12 - 5)) + 5;
+    }
+  }
+});
+
+app.mount('#game');
